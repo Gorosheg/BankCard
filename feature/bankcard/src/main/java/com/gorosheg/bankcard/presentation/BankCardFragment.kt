9bccbd5640c99bbb05a3a4b8a0b6d977a2179a4a
AnnaG <anutagoroxowa@yandex.ru>
2022-12-21 14:12:47 +0200

    Add Bank card fragment

---
+++ feature/bankcard/src/main/java/com/gorosheg/bankcard/presentation/BankCardFragment.kt
@@ -0,0 +1,7 @@
+package com.gorosheg.bankcard.presentation
+
+import androidx.fragment.app.Fragment
+import com.gorosheg.bankcard.R
+
+class BankCardFragment : Fragment(R.layout.fragment_bank_card) {
+}
